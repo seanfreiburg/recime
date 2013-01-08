e0ccee0c34e6c52283dcf03e08f3e513d991f8ab
Sean Freiburg <freibur1@illinois.edu>
2013-01-07 23:23:46 -0600

    not sure if anything changed

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,6 +15,8 @@ describe "User pages" do
 
     it { should have_selector('title', text: 'All users') }
     it { should have_selector('h1',    text: 'All users') }
+    
+    
 
     describe "pagination" do
 
@@ -91,7 +93,7 @@ describe "User pages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
